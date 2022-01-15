57a7e8fbeaf419cde825204846736be85a5b094b
venesa okuna <venesa.okuna@student.moringaschool.com>
2022-01-15 11:07:30 +0300

    This commit will add github app component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubAppComponent } from './github-app/github-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubAppComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/github-app/github-app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-github-app',
+  templateUrl: './github-app.component.html',
+  styleUrls: ['./github-app.component.css']
+})
+export class GithubAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
