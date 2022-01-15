3ccb8ace146c812f14746bf65ad44369d3c6e5d0
venesa okuna <venesa.okuna@student.moringaschool.com>
2022-01-15 11:11:06 +0300

    This commit will add profile-card component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubAppComponent } from './github-app/github-app.component';
 import { GithubProfileComponent } from './github-profile/github-profile.component';
+import { GithubProfileCardComponent } from './github-profile-card/github-profile-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GithubAppComponent,
-    GithubProfileComponent
+    GithubProfileComponent,
+    GithubProfileCardComponent
   ],
   imports: [
     BrowserModule,
