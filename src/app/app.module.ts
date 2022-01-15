bd552e9180b3b13d9ad1d76931e6640e07d9f565
venesa okuna <venesa.okuna@student.moringaschool.com>
2022-01-15 11:09:15 +0300

    This commit will add github profile component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubAppComponent } from './github-app/github-app.component';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GithubAppComponent
+    GithubAppComponent,
+    GithubProfileComponent
   ],
   imports: [
     BrowserModule,
